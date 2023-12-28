60c2469ddd783a84e7378328990f26f90eab633c
Prince Rachit Sinha <prs@sinhasoftware.solutions>
2023-12-29 00:31:53 +0530

    Add remaining methods, tests and benchmark code

--- node.go
+++ node.go
@@ -10,6 +10,7 @@ type Graph struct {
 	mutex *sync.RWMutex
 }
 
+// NewGraph returns a new Graph with empty nodes
 func NewGraph() *Graph {
 	return &Graph{
 		Nodes: make(map[string]Hashset),
@@ -17,13 +18,15 @@ func NewGraph() *Graph {
 	}
 }
 
+// AddOrReplaceNode adds or replaces a node in the graph
 func (g *Graph) AddOrReplaceNode(k string, v Hashset) {
 	g.mutex.Lock()
 	defer g.mutex.Unlock()
 	g.Nodes[k] = v
 }
 
-func (g *Graph) RemoveNode(k string) {
+// PruneNode removes a node from the graph. It also removes all edges
+func (g *Graph) PruneNode(k string) {
 	g.mutex.Lock()
 	defer g.mutex.Unlock()
 	delete(g.Nodes, k)
@@ -32,6 +35,14 @@ func (g *Graph) RemoveNode(k string) {
 	}
 }
 
+// PruneNodes removes an array of nodes from the graph. It also removes all edges.
+func (g *Graph) PruneNodes(nodes []string) {
+	for _, node := range nodes {
+		g.PruneNode(node)
+	}
+}
+
+// GetPruneCandidates returns an array of nodes that have no children
 func (g *Graph) GetPruneCandidates() []string {
 	g.mutex.RLock()
 	defer g.mutex.RUnlock()
--- node_test.go
+++ node_test.go
@@ -55,27 +55,165 @@ func TestGraph_AddOrReplaceNode(t *testing.T) {
 	})
 }
 
-func TestGraph_RemoveNode(t *testing.T) {
-	t.Run("Key already exists and gets removed", func(t *testing.T) {
+func TestGraph_PruneNode(t *testing.T) {
+	t.Run("Node already exists and gets removed", func(t *testing.T) {
 		g := &Graph{
 			Nodes: map[string]Hashset{
 				"a": {"b": true},
 			},
 			mutex: new(sync.RWMutex),
 		}
-		g.RemoveNode("a")
+		g.PruneNode("a")
 		if _, ok := g.Nodes["a"]; ok {
-			t.Errorf("RemoveNode() expected to remove the key but it still exists")
+			t.Errorf("PruneNode() expected to remove the Node but it still exists")
 		}
 	})
-	t.Run("Key does not exist and continues to not exist", func(t *testing.T) {
+	t.Run("Node does not exist and continues to not exist", func(t *testing.T) {
 		g := &Graph{
 			Nodes: make(map[string]Hashset),
 			mutex: new(sync.RWMutex),
 		}
-		g.RemoveNode("a")
+		g.PruneNode("a")
 		if _, ok := g.Nodes["a"]; ok {
-			t.Errorf("RemoveNode() expected to remove the key but it exists")
+			t.Errorf("PruneNode() expected to remove the Node but it exists")
 		}
 	})
+	t.Run("Node exists and there are edges to the node which are removed too", func(t *testing.T) {
+		g := &Graph{
+			Nodes: map[string]Hashset{
+				"a": {"b": true, "d": true},
+				"b": {},
+			},
+			mutex: new(sync.RWMutex),
+		}
+		g.PruneNode("b")
+		if _, ok := g.Nodes["b"]; ok {
+			t.Errorf("PruneNode() expected to remove the Node but it still exists")
+		}
+		if _, ok := g.Nodes["a"]["b"]; ok {
+			t.Errorf("PruneNode() expected to remove the edge to Node but it still exists")
+		}
+		if _, ok := g.Nodes["a"]["d"]; !ok {
+			t.Errorf("PruneNode() expected to not remove the edge to other nodes but removed")
+		}
+	})
+}
+
+func TestGraph_GetPruneCandidates(t *testing.T) {
+	t.Run("Pruning candidates exist and are returned", func(t *testing.T) {
+		g := createTestGraph()
+		expected := []string{"c", "d"}
+		if got := g.GetPruneCandidates(); !reflect.DeepEqual(got, expected) {
+			t.Errorf("GetPruneCandidates() = %v, want %v", got, expected)
+		}
+	})
+
+	t.Run("Pruning candidates don't exist so empty slice is returned", func(t *testing.T) {
+		g := createTestGraph()
+		delete(g.Nodes, "c")
+		delete(g.Nodes, "d")
+		var expected []string
+		if got := g.GetPruneCandidates(); !reflect.DeepEqual(got, expected) {
+			t.Errorf("GetPruneCandidates() = %v, want %v", got, expected)
+		}
+	})
+}
+
+func TestGraph_PruneNodes(t *testing.T) {
+	t.Run("Nodes exists and there are edges to the node which are removed too", func(t *testing.T) {
+		g := &Graph{
+			Nodes: map[string]Hashset{
+				"a": {"b": true, "d": true, "e": true},
+				"c": {"b": true, "d": true, "f": true},
+				"b": {},
+				"d": {},
+			},
+			mutex: new(sync.RWMutex),
+		}
+		g.PruneNodes([]string{"b", "d"})
+		if _, ok := g.Nodes["b"]; ok {
+			t.Errorf("PruneNodes() expected to remove the Node b but it still exists")
+		}
+		if _, ok := g.Nodes["d"]; ok {
+			t.Errorf("PruneNodes() expected to remove the Node d but it still exists")
+		}
+		if _, ok := g.Nodes["a"]["b"]; ok {
+			t.Errorf("PruneNodes() expected to remove the edge to Node b but it still exists")
+		}
+		if _, ok := g.Nodes["a"]["d"]; ok {
+			t.Errorf("PruneNodes() expected to remove the edge to Node d but it still exists")
+		}
+		if _, ok := g.Nodes["a"]["e"]; !ok {
+			t.Errorf("PruneNodes() expected to not remove the edge to other Node e but removed")
+		}
+		if _, ok := g.Nodes["c"]["f"]; !ok {
+			t.Errorf("PruneNodes() expected to not remove the edge to other Node f but removed")
+		}
+	})
+}
+
+func createTestGraph() Graph {
+	return Graph{
+		Nodes: map[string]Hashset{
+			"a": {"b": true, "c": true},
+			"b": {"c": true, "d": true},
+			"c": {},
+			"d": {},
+		},
+		mutex: new(sync.RWMutex),
+	}
+}
+
+func BenchmarkGraph_AddOrReplaceNode(b *testing.B) {
+	g := createBenchmarkGraph()
+	for i := 0; i < b.N; i++ {
+		g.AddOrReplaceNode("a", Hashset{"b": true})
+	}
+}
+
+func BenchmarkGraph_PruneNode(b *testing.B) {
+	g := createBenchmarkGraph()
+	for i := 0; i < b.N; i++ {
+		g.PruneNode("static1")
+	}
+}
+
+func BenchmarkGraph_GetPruneCandidates(b *testing.B) {
+	g := createBenchmarkGraph()
+	for i := 0; i < b.N; i++ {
+		g.GetPruneCandidates()
+	}
+}
+
+func createBenchmarkGraph() Graph {
+	return Graph{
+		Nodes: map[string]Hashset{
+			"a":       {"b": true, "c": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"b":       {"c": true, "d": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"c":       {"d": true, "e": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"d":       {"e": true, "f": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"e":       {"f": true, "g": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"f":       {"g": true, "h": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"g":       {"h": true, "i": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"h":       {"i": true, "j": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"i":       {"j": true, "k": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"j":       {"k": true, "l": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"k":       {"l": true, "m": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"l":       {"m": true, "n": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"m":       {"n": true, "o": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"n":       {"o": true, "p": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"o":       {"p": true, "q": true, "static1": true, "static2": true, "static4": true, "static5": true, "empty1": true, "empty2": true},
+			"empty1":  {},
+			"empty2":  {},
+			"empty3":  {},
+			"empty4":  {},
+			"empty5":  {},
+			"empty6":  {},
+			"empty7":  {},
+			"empty8":  {},
+			"empty9":  {},
+			"empty10": {},
+		},
+		mutex: new(sync.RWMutex),
+	}
 }
